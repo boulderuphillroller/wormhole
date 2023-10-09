8915603d2cf6382801ad7ebfe33570227f97331a
Bruce Riley <bruce@w7.xyz>
2023-10-09 16:25:37 -0500

    Deploy scroll to testnet

--- node/cmd/guardiand/node.go
+++ node/cmd/guardiand/node.go
@@ -174,6 +174,9 @@ var (
 	baseRPC      *string
 	baseContract *string
 
+	scrollRPC      *string
+	scrollContract *string
+
 	sepoliaRPC      *string
 	sepoliaContract *string
 
@@ -336,6 +339,9 @@ func init() {
 	optimismRPC = NodeCmd.Flags().String("optimismRPC", "", "Optimism RPC URL")
 	optimismContract = NodeCmd.Flags().String("optimismContract", "", "Optimism contract address")
 
+	scrollRPC = NodeCmd.Flags().String("scrollRPC", "", "Scroll RPC URL")
+	scrollContract = NodeCmd.Flags().String("scrollContract", "", "Scroll contract address")
+
 	baseRPC = NodeCmd.Flags().String("baseRPC", "", "Base RPC URL")
 	baseContract = NodeCmd.Flags().String("baseContract", "", "Base contract address")
 
@@ -488,6 +494,7 @@ func runNode(cmd *cobra.Command, args []string) {
 		*optimismContract = unsafeDevModeEvmContractAddress(*optimismContract)
 		*baseContract = unsafeDevModeEvmContractAddress(*baseContract)
 		*sepoliaContract = unsafeDevModeEvmContractAddress(*sepoliaContract)
+		*scrollContract = unsafeDevModeEvmContractAddress(*scrollContract)
 	}
 
 	// Verify flags
@@ -619,6 +626,10 @@ func runNode(cmd *cobra.Command, args []string) {
 
 	if (*baseRPC == "") != (*baseContract == "") {
 		logger.Fatal("Both --baseContract and --baseRPC must be set together or both unset")
+	}
+
+	if (*scrollRPC == "") != (*scrollContract == "") {
+		logger.Fatal("Both --scrollContract and --scrollRPC must be set together or both unset")
 	}
 
 	if *gatewayWS != "" {
@@ -861,6 +872,7 @@ func runNode(cmd *cobra.Command, args []string) {
 	if env == common.TestNet {
 		rpcMap["sepoliaRPC"] = *sepoliaRPC
 	}
+	rpcMap["scrollRPC"] = *scrollRPC
 	rpcMap["solanaRPC"] = *solanaRPC
 	rpcMap["suiRPC"] = *suiRPC
 	rpcMap["terraWS"] = *terraWS
@@ -1223,6 +1235,17 @@ func runNode(cmd *cobra.Command, args []string) {
 			ChainID:   vaa.ChainIDBase,
 			Rpc:       *baseRPC,
 			Contract:  *baseContract,
+		}
+
+		watcherConfigs = append(watcherConfigs, wc)
+	}
+
+	if shouldStart(scrollRPC) {
+		wc := &evm.WatcherConfig{
+			NetworkID: "scroll",
+			ChainID:   vaa.ChainIDScroll,
+			Rpc:       *scrollRPC,
+			Contract:  *scrollContract,
 		}
 
 		watcherConfigs = append(watcherConfigs, wc)
